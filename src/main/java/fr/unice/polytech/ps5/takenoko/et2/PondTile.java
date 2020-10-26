c9a735d30c51b65005ae8263aa784a29852fe662
Tom Niget <tom.niget@etu.univ-cotedazur.fr>
2020-10-26 18:33:23 +0100

    Add pond tile.

---
+++ src/main/java/fr/unice/polytech/ps5/takenoko/et2/PondTile.java
@@ -0,0 +1,15 @@
+package fr.unice.polytech.ps5.takenoko.et2;
+
+/**
+ * Pond tile. This tile can only be present once on the game board, at the center.
+ */
+public class PondTile extends Tile
+{
+    public PondTile()
+    {
+        for (int i = 0; i < this.edges.length; i++)
+        {
+            this.edges[i] = new Edge(this);
+        }
+    }
+}
