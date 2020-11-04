eb91c041d489dc748685c3727b20038de3986f70
Thomas Prévost <thomas.prevost@etu.univ-cotedazur.fr>
2020-11-04 17:49:40 +0100

    PlotObjective works... maybe :/ #9

--- src/main/java/fr/unice/polytech/ps5/takenoko/et2/objective/PlotObjective.java
+++ src/main/java/fr/unice/polytech/ps5/takenoko/et2/objective/PlotObjective.java
@@ -8,16 +8,37 @@ import java.util.Map;
 
 public class PlotObjective extends Objective
 {
-    private PlotObjectivePath plotObjectivePath;
-    private List<Color> listColors = new ArrayList<Color>();
-    private List<TilePosition> listPaths = new ArrayList<>();
+    private final List<Color> listColors = new ArrayList<>();
+    private final List<Integer> listPaths = new ArrayList<>();
 
-    public PlotObjective(int points)
+    public PlotObjective(List<Color> listC, List<Integer> listP, int points) throws Exception
     {
         super(points);
+        if (listC.size() == 0 || listC.size() != (listP.size() + 1))
+        {
+            throw new Exception("Invalid Objective");
+        }
+        listColors.addAll(listC);
+        listPaths.addAll(listP);
     }
 
     public boolean checkValidated(Board board)
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            for (int j = 0; j < listPaths.size(); j++)
+            {
+                listPaths.set(j, (listPaths.get(j) + i) % 6);
+            }
+            if (checkValidatedSpecificRotation(board))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkValidatedSpecificRotation(Board board)
     {
         Map<TilePosition, Tile> listTiles = board.getTiles();
         for (Map.Entry<TilePosition, Tile> entry : listTiles.entrySet())
@@ -29,25 +50,30 @@ public class PlotObjective extends Objective
             LandTile landTile = (LandTile) entry.getValue();
             if (landTile.getColor() == listColors.get(0))
             { // this is a match^^
-                //
+                int i = 0;
+                for (Integer nextEdgePosition : listPaths)
+                {
+                    Tile landTile2 = landTile.getEdge(nextEdgePosition).getTile(0);
+                    if (landTile2 instanceof PondTile)
+                    {
+                        return false;
+                    }
+                    if (landTile2 == landTile)
+                    {
+                        landTile2 = landTile.getEdge(nextEdgePosition).getTile(1);
+                        if (landTile2 instanceof PondTile)
+                        {
+                            return false;
+                        }
+                        landTile = (LandTile) landTile2;
+                        if (landTile.getColor() != listColors.get(i))
+                        {
+                            return false;
+                        }
+                    }
+                    i++;
+                }
             }
-        }
-        return false;
-    }
-
-    public boolean setObjectivePath(List<Color> listC, List<TilePosition> listP)
-    {
-        if (listC.size() == 0 || listC.size() != (listP.size() + 1))
-        {
-            return false;
-        }
-        for (Color color : listC)
-        {
-            listColors.add(color);
-        }
-        for (TilePosition tp : listP)
-        {
-            listPaths.add(tp);
         }
         return true;
     }
