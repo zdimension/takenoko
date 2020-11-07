581b0955dffaa5d93e66e75024dacca3ef6d25e7
Emmeline Vouriot <emmeline.vouriot@etu.univ-cotedazur.fr>
2020-11-07 20:22:28 +0100

    Add new Exception type

---
+++ src/main/java/fr/unice/polytech/ps5/takenoko/et2/DecisionMakerException.java
@@ -0,0 +1,9 @@
+package fr.unice.polytech.ps5.takenoko.et2;
+
+public class DecisionMakerException extends Exception
+{
+    public DecisionMakerException(String message)
+    {
+        super(message);
+    }
+}
