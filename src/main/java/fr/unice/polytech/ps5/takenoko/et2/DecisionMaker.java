e0cfce1149f531e7a8958a2ae7b20aa886cc5a54
Emmeline Vouriot <emmeline.vouriot@etu.univ-cotedazur.fr>
2020-11-07 20:25:49 +0100

    add partial game logic #11 #12

--- src/main/java/fr/unice/polytech/ps5/takenoko/et2/DecisionMaker.java
+++ src/main/java/fr/unice/polytech/ps5/takenoko/et2/DecisionMaker.java
@@ -1,5 +1,10 @@
 package fr.unice.polytech.ps5.takenoko.et2;
 
+import fr.unice.polytech.ps5.takenoko.et2.objective.Objective;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class DecisionMaker
 {
     protected Player player;
@@ -14,5 +19,48 @@ public abstract class DecisionMaker
         return player.getGame().getBoard();
     }
 
-    //abstract GameAction chooseAction();
+    /**
+     *
+     * @return chosen action
+     */
+    abstract GameAction chooseAction();
+
+    /**
+     *
+     * @return true if DecisionMaker wants to do an extra action, false otherwise
+     */
+    abstract boolean anyExtraAction();
+
+    /**
+     *
+     * @return chosen extra action
+     */
+    abstract GameAction chooseExtraAction();
+
+    /**
+     *
+     * @return class of the deck chosen
+     */
+    abstract Class<? extends Objective> chooseDeck();
+
+    /**
+     *
+     * @param drawnTiles to choose from
+     * @return chosen tile
+     */
+    abstract LandTile chooseTile(List<LandTile> drawnTiles); //drawnTiles.size() = 3
+
+    /**
+     *
+     * @param tile to put on the board
+     * @return desired position of tile
+     */
+    abstract TilePosition chooseTilePosition(LandTile tile);
+
+    /**
+     *
+     * @return Objective to complete
+     */
+    abstract Objective completeObjective();
+
 }
--- src/main/java/fr/unice/polytech/ps5/takenoko/et2/Game.java
+++ src/main/java/fr/unice/polytech/ps5/takenoko/et2/Game.java
@@ -1,30 +1,34 @@
 package fr.unice.polytech.ps5.takenoko.et2;
 
+import fr.unice.polytech.ps5.takenoko.et2.objective.Objective;
 import fr.unice.polytech.ps5.takenoko.et2.objective.PlotObjective;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class Game
 {
     private static int numberActionsInTurn = 2;
     private Board board;
-    private List<PlotObjective> plotObjectiveDeck;
+    private Map<Class<? extends Objective>, List<? extends Objective>> objectiveDecks = new HashMap<>();
+
     private List<LandTile> tileDeck;
     private ArrayList<Player> playerList;
-    private boolean isFirstTurn;
-    private boolean isLastTurn;
+    private boolean isFirstRound;
+    private static int numberObjectivesToWin = 9;
+    private boolean emperorTriggered;
 
-    public Game(HashMap<Player, DecisionMaker> playerMap)
+    public Game(List<PlotObjective> plotObjectiveDeck, List<LandTile> tileDeck)
     {
+        if (plotObjectiveDeck.size() == 0)
+        {
+            throw new IllegalArgumentException("PlotObjective deck is empty");
+        }
         playerList = new ArrayList<Player>();
-        //initialiser les listes
-        isFirstTurn = true;
-        isLastTurn = false;
+        isFirstRound = true;
         board = new Board();
-        plotObjectiveDeck = new ArrayList<PlotObjective>();
-        tileDeck = new ArrayList<LandTile>();
+        this.objectiveDecks.put(PlotObjective.class, plotObjectiveDeck);
+        this.tileDeck = tileDeck;
+        this.emperorTriggered = false;
     }
 
     void addPlayer(DecisionMakerBuilder builder)
@@ -32,26 +36,117 @@ public class Game
         this.playerList.add(new Player(this, builder));
     }
 
-    public void gameProcessing()
+    public void gameProcessing() throws DecisionMakerException
     {
+
         if (playerList.size() < 2)
         {
             throw new IllegalArgumentException("Game started with less than 2 players");
         }
-        //deroulement de la partie
+        if (this.tileDeck.size() == 0)
+        {
+            throw new IllegalArgumentException("Game started with empty tile deck");
+        }
 
-        //une grande boucle tour
-
-        //si le dernier joueur a joué et que c'etait le premier tour le champ passe à flase
-        //si le dernier joueur a joué et que c'était le dernier tour, on quitte la boucle
+        objectiveDecks.values().forEach(Collections::shuffle);
+        Collections.shuffle(tileDeck);
 
 
-        //on check qui gagne
+        int numberPlayers = playerList.size() - 1;
+        int i = 0;
+
+        do
+        {
+            var player = playerList.get(i);
+            if (player.isHasTriggeredEmperor())
+            {
+                break;
+            }
+
+            var dm = player.getDecisionMaker();
+
+            GameAction chosenAction = dm.chooseAction();
+
+            for (int j = numberActionsInTurn; j > 0; )
+            {
+                switch (chosenAction)
+                {
+                    case DRAW_OBJECTIVE:
+                        if (player.getHand().size() >= 5)
+                        {
+                            continue;
+                        }
+
+                        var clazz = dm.chooseDeck();
+                        if (clazz == null)
+                        {
+                            continue;
+                        }
+                        player.addObjective(objectiveDecks.get(clazz).get(0));
+                        objectiveDecks.get(clazz).remove(0); //TODO can a deck be empty?
+                        break;
+
+                    case DRAW_TILE:
+                        LandTile chosenTile = dm.chooseTile(Collections.unmodifiableList(tileDeck.subList(0, 3)));
+                        TilePosition tilePosition = dm.chooseTilePosition(chosenTile);
+                        if (!board.isValid(tilePosition))
+                        {
+                            continue;
+                        }
+                        board.addTile(chosenTile, tilePosition);
+                        tileDeck.remove(chosenTile);
+                        break;
+
+                    case COMPLETE_OBJECTIVE:
+                        this.completeObjective(player);
+                        break;
+                    default:
+                        throw new DecisionMakerException("Value of chosenAction does not conform to available values");
+                }
+                if (!chosenAction.isUnlimited())
+                {
+                    j--;
+                }
+            }
+
+            while (dm.anyExtraAction())
+            {
+                GameAction chosenExtraAction = dm.chooseExtraAction();
+                switch (chosenExtraAction)
+                {
+                    case COMPLETE_OBJECTIVE:
+                        this.completeObjective(player);
+                        break;
+                    default:
+                        throw new DecisionMakerException("Value of chosenExtraAction does not conform to available values");
+                }
+            }
+
+
+            if (i == numberPlayers - 1)
+            {
+                i = 0;
+            }
+            else
+            {
+                i++;
+                //if(isFirstRound){
+                //    isFirstRound = false;
+                //}
+            }
+        }
+        while (true);
+        ArrayList<Player> winners = whoWins();
+        winners.forEach(w -> System.out.println(w));
     }
 
+    /**
+     * @return list of winning players, empty list if no one won
+     */
     private ArrayList<Player> whoWins()
     {
         int bestScore = 0;
+        ArrayList<Player> winners = new ArrayList<Player>();
         for (Player player : playerList)
         {
             if (bestScore <= player.countPoints())
@@ -59,7 +154,11 @@ public class Game
                 bestScore = player.countPoints();
             }
         }
-        ArrayList<Player> winners = new ArrayList<Player>();
+        if (bestScore < Game.numberObjectivesToWin)
+        {
+            return winners;
+        }
+
         for (Player player : playerList)
         {
             if (bestScore == player.countPoints())
@@ -75,4 +174,24 @@ public class Game
     {
         return board;
     }
+
+    private void completeObjective(Player player)
+    {
+        //on regarde si l'objectif est complet
+        //si oui on le retire de la main pour mettre dans la liste des objectifs finis
+        //on calcule le nb de points
+        //TODO
+
+
+        if (player.countPoints() >= numberObjectivesToWin)
+        {
+            if (emperorTriggered)
+            {
+                return;
+            }
+            player.triggerEmperor();
+        }
+    }
+
+    //public getPlayerIndividualBoard(PLayer player)
 }
