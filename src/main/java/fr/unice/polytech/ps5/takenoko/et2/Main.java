ef75c687bed02db7dfca85e1c355d89aca87955e
Tom Niget <tom.niget@etu.univ-cotedazur.fr>
2020-11-08 17:51:17 +0100

    frequency tester

--- src/main/java/fr/unice/polytech/ps5/takenoko/et2/Main.java
+++ src/main/java/fr/unice/polytech/ps5/takenoko/et2/Main.java
@@ -1,10 +1,12 @@
 package fr.unice.polytech.ps5.takenoko.et2;
 
 import fr.unice.polytech.ps5.takenoko.et2.board.LandTile;
+import fr.unice.polytech.ps5.takenoko.et2.decision.DecisionMakerBuilder;
 import fr.unice.polytech.ps5.takenoko.et2.decision.bots.RandomBot;
 import fr.unice.polytech.ps5.takenoko.et2.objective.PlotObjective;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main
@@ -12,27 +14,39 @@ public class Main
     public static void main(String... args) throws Exception
     {
         var land = new ArrayList<LandTile>();
-        for (var i = 0; i < 11; i++)
+        for (var i = 0; i < 110; i++)
+        {
             land.add(new LandTile(Color.GREEN));
-        for (var i = 0; i < 9; i++)
+        }
+        for (var i = 0; i < 90; i++)
+        {
             land.add(new LandTile(Color.YELLOW));
-        for (var i = 0; i < 7; i++)
+        }
+        for (var i = 0; i < 70; i++)
+        {
             land.add(new LandTile(Color.PINK));
-        var game = new Game(new ArrayList<>(List.of(
+        }
+        var objectives = List.of(
             new PlotObjective(
                 2, List.of(Color.GREEN, Color.GREEN, Color.GREEN), List.of(0, 2)
+            ),
+            new PlotObjective(
+                2, List.of(Color.GREEN, Color.GREEN, Color.GREEN), List.of(5, 0)
+            ),
+            new PlotObjective(
+                2, List.of(Color.GREEN, Color.GREEN, Color.GREEN), List.of(5, 5)
             ),
             new PlotObjective(
                 3, List.of(Color.GREEN, Color.GREEN, Color.GREEN, Color.GREEN), List.of(0, 2, 3)
             ),
             new PlotObjective(
-                5, List.of(Color.PINK, Color.PINK, Color.YELLOW, Color.YELLOW), List.of(2, 3, 5)
-            ),
-            new PlotObjective(
-                4, List.of(Color.GREEN, Color.GREEN, Color.PINK, Color.PINK), List.of(2, 3, 5)
-            ),
-            new PlotObjective(
                 3, List.of(Color.GREEN, Color.GREEN, Color.YELLOW, Color.YELLOW), List.of(2, 3, 5)
+            ),
+            new PlotObjective(
+                3, List.of(Color.YELLOW, Color.YELLOW, Color.YELLOW), List.of(0, 2)
+            ),
+            new PlotObjective(
+                3, List.of(Color.YELLOW, Color.YELLOW, Color.YELLOW), List.of(5, 0)
             ),
             new PlotObjective(
                 3, List.of(Color.YELLOW, Color.YELLOW, Color.YELLOW), List.of(5, 5)
@@ -41,33 +55,49 @@ public class Main
                 4, List.of(Color.YELLOW, Color.YELLOW, Color.YELLOW, Color.YELLOW), List.of(0, 2, 3)
             ),
             new PlotObjective(
-                3, List.of(Color.YELLOW, Color.YELLOW, Color.YELLOW), List.of(5, 0)
-            ),
-            new PlotObjective(
-                2, List.of(Color.GREEN, Color.GREEN, Color.GREEN), List.of(5, 5)
-            ),
-            new PlotObjective(
-                2, List.of(Color.GREEN, Color.GREEN, Color.GREEN), List.of(5, 0)
-            ),
-            new PlotObjective(
-                4, List.of(Color.PINK, Color.PINK, Color.PINK), List.of(5, 0)
+                4, List.of(Color.GREEN, Color.GREEN, Color.PINK, Color.PINK), List.of(2, 3, 5)
             ),
             new PlotObjective(
                 4, List.of(Color.PINK, Color.PINK, Color.PINK), List.of(0, 2)
             ),
             new PlotObjective(
-                5, List.of(Color.PINK, Color.PINK, Color.PINK, Color.PINK), List.of(0, 2, 3)
+                4, List.of(Color.PINK, Color.PINK, Color.PINK), List.of(5, 0)
             ),
             new PlotObjective(
                 4, List.of(Color.PINK, Color.PINK, Color.PINK), List.of(5, 5)
             ),
             new PlotObjective(
-                3, List.of(Color.YELLOW, Color.YELLOW, Color.YELLOW), List.of(0, 2)
+                5, List.of(Color.PINK, Color.PINK, Color.PINK, Color.PINK), List.of(0, 2, 3)
+            ),
+            new PlotObjective(
+                5, List.of(Color.PINK, Color.PINK, Color.YELLOW, Color.YELLOW), List.of(2, 3, 5)
             )
-        )), land);
-        //game.addPlayer(DecisionMakerDebugger::new);
-        game.addPlayer(RandomBot::new);
-        game.addPlayer(RandomBot::new);
-        game.gameProcessing();
+        );
+        var players = List.<DecisionMakerBuilder>of(
+            RandomBot::new,
+            RandomBot::new,
+            RandomBot::new,
+            RandomBot::new
+        );
+        var freq = new int[players.size()];
+        var N = 1000;
+        for (var i = 0; i < N; i++)
+        {
+            if (i % 1000 == 0)
+            {
+                System.out.println(i);
+            }
+            var game = new Game(objectives, land);
+            for (DecisionMakerBuilder player : players)
+            {
+                game.addPlayer(player);
+            }
+            var res = game.gameProcessing();
+            for (Integer re : res)
+            {
+                freq[re]++;
+            }
+        }
+        System.out.println(Arrays.toString(Arrays.stream(freq).asDoubleStream().map(d -> d / N).toArray()));
     }
 }
