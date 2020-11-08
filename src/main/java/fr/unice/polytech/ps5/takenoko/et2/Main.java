cb0765749fb4ee834b8b51a522885d59291be536
Tom Niget <tom.niget@etu.univ-cotedazur.fr>
2020-11-08 16:35:50 +0100

    remove testing code

--- src/main/java/fr/unice/polytech/ps5/takenoko/et2/Main.java
+++ src/main/java/fr/unice/polytech/ps5/takenoko/et2/Main.java
@@ -18,32 +18,6 @@ import java.util.List;
 
 public class Main
 {
-
-    static int nPoss = 0;
-
-    static void checkPossRec(Board board, int n, int minX, int maxX, int minY, int maxY)
-    {
-        if (n == 0)
-        {
-            nPoss++;
-            return;
-        }
-        for (int i = minX; i <= maxX; i++)
-        {
-            for (int j = minY; j <= maxY; j++)
-            {
-                Board boardBis = new Board();
-                for (Map.Entry<TilePosition, Tile> entry : board.getTiles().entrySet()) // For each Tile
-                {
-                    boardBis.addTile(new LandTile(Color.GREEN), entry.getKey());
-                }
-                if (boardBis.addTile(new LandTile(Color.GREEN), new TilePosition(i, j)))
-                {
-                    checkPossRec(boardBis, n - 1, ((i == minX) ? (minX - 1) : minX), ((i == maxX) ? (maxX + 1) : maxX), ((j == minY) ? (minY - 1) : minY), ((j == maxY) ? (maxY + 1) : maxY));
-                }
-            }
-        }
-    }
     public static void main(String... args) throws Exception
     {
         var land = new ArrayList<LandTile>();
@@ -134,10 +108,5 @@ public class Main
         game.addPlayer(RandomBot::new);
         game.addPlayer(RandomBot::new);
         game.gameProcessing();
-        Board board = new Board();
-        int n = 8;
-        checkPossRec(board, n, -1, 1, -1, 1);
-        System.out.println(nPoss);
-        System.out.println(nPoss * Math.pow((double) 3, (double) n));
     }
 }
