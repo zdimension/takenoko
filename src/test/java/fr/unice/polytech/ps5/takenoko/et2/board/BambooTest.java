ab30a1fb0890a6e2f3ffc4a7c8adf35e04948aa9
Emmeline Vouriot <emmeline.vouriot@etu.univ-cotedazur.fr>
2020-11-18 22:09:56 +0100

    Add test #5

--- src/test/java/fr/unice/polytech/ps5/takenoko/et2/board/BambooTest.java
+++ src/test/java/fr/unice/polytech/ps5/takenoko/et2/board/BambooTest.java
@@ -2,7 +2,6 @@ package fr.unice.polytech.ps5.takenoko.et2.board;
 
 import fr.unice.polytech.ps5.takenoko.et2.BambooSection;
 import fr.unice.polytech.ps5.takenoko.et2.Color;
-import fr.unice.polytech.ps5.takenoko.et2.decision.bots.RandomBot;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -15,11 +14,23 @@ class BambooTest
 {
     Board board;
     List<BambooSection> bambooReserve;
+    LandTile l1;
+    LandTile l2;
+    LandTile l3;
+    LandTile l4;
+    LandTile l5;
+    LandTile l6;
+    LandTile l7;
+    LandTile l8;
+    LandTile l9;
+    LandTile l10;
+    LandTile l11;
+    LandTile l12;
 
     @BeforeEach
     void init()
     {
-        bambooReserve = new ArrayList<BambooSection>();
+        bambooReserve = new ArrayList<>();
         int i;
         for (i = 0; i < 30; i++)
         {
@@ -30,6 +41,31 @@ class BambooTest
         try
         {
             board = new Board();
+
+            l1 = new LandTile(Color.GREEN);
+            l2 = new LandTile(Color.YELLOW);
+            l3 = new LandTile(Color.PINK);
+            l4 = new LandTile(Color.PINK);
+            l5 = new LandTile(Color.GREEN);
+            l6 = new LandTile(Color.YELLOW);
+            board.addTile(l1, new TilePosition(0, 1), bambooReserve);
+            board.addTile(l2, new TilePosition(1, 0), bambooReserve);
+            board.addTile(l3, new TilePosition(1, -1), bambooReserve);
+            board.addTile(l4, new TilePosition(0, -1), bambooReserve);
+            board.addTile(l5, new TilePosition(-1, 0), bambooReserve);
+            board.addTile(l6, new TilePosition(-1, 1), bambooReserve);
+            l7 = new LandTile(Color.GREEN);
+            l8 = new LandTile(Color.YELLOW);
+            l9 = new LandTile(Color.PINK);
+            l10 = new LandTile(Color.PINK);
+            l11 = new LandTile(Color.GREEN);
+            l12 = new LandTile(Color.YELLOW);
+            board.addTile(l7, new TilePosition(1, 1), bambooReserve);
+            board.addTile(l8, new TilePosition(2, -1), bambooReserve);
+            board.addTile(l9, new TilePosition(1, -2), bambooReserve);
+            board.addTile(l10, new TilePosition(-1, -1), bambooReserve);
+            board.addTile(l11, new TilePosition(-2, 1), bambooReserve);
+            board.addTile(l12, new TilePosition(-1, 2), bambooReserve);
         }
         catch (Exception e)
         {
@@ -41,32 +77,6 @@ class BambooTest
     @Test
     void growBambooSectionYellowTest() throws Exception
     {
-        //alors il va falloir instancier tout un plateau avec des irrigztions
-        LandTile l1 = new LandTile(Color.GREEN);
-        LandTile l2 = new LandTile(Color.YELLOW);
-        LandTile l3 = new LandTile(Color.PINK);
-        LandTile l4 = new LandTile(Color.PINK);
-        LandTile l5 = new LandTile(Color.GREEN);
-        LandTile l6 = new LandTile(Color.YELLOW);
-        board.addTile(l1, new TilePosition(0, 1), bambooReserve);
-        board.addTile(l2, new TilePosition(1, 0), bambooReserve);
-        board.addTile(l3, new TilePosition(1, -1), bambooReserve);
-        board.addTile(l4, new TilePosition(0, -1), bambooReserve);
-        board.addTile(l5, new TilePosition(-1, 0), bambooReserve);
-        board.addTile(l6, new TilePosition(-1, 1), bambooReserve);
-        LandTile l7 = new LandTile(Color.GREEN);
-        LandTile l8 = new LandTile(Color.YELLOW);
-        LandTile l9 = new LandTile(Color.PINK);
-        LandTile l10 = new LandTile(Color.PINK);
-        LandTile l11 = new LandTile(Color.GREEN);
-        LandTile l12 = new LandTile(Color.YELLOW);
-        board.addTile(l7, new TilePosition(1, 1), bambooReserve);
-        board.addTile(l8, new TilePosition(2, -1), bambooReserve);
-        board.addTile(l9, new TilePosition(1, -2), bambooReserve);
-        board.addTile(l10, new TilePosition(-1, -1), bambooReserve);
-        board.addTile(l11, new TilePosition(-2, 1), bambooReserve);
-        board.addTile(l12, new TilePosition(-1, 2), bambooReserve);
-
         for (int i = 0; i < 3; i++)
         {
 
@@ -90,7 +100,50 @@ class BambooTest
         l2.growBambooSection(bambooSection);
         assertEquals(4, l2.getBambooSize());
 
+    }
 
+    @Test
+    void growBambooSectionFalseTest() throws Exception
+    {
+        var res = this.bambooReserve.stream().filter(b -> b.getColor().equals(l10.getColor())).findAny();
+        if (res.isEmpty())
+        {
+            return;
+        }
+        var bambooSection = res.get();
+        assertFalse(l10.growBambooSection(bambooSection));
 
+        for (int i = 0; i < 3; i++)
+        {
+
+            var res1 = this.bambooReserve.stream().filter(b -> b.getColor().equals(l2.getColor())).findAny();
+            if (res1.isEmpty())
+            {
+                return;
+            }
+            var bambooSection1 = res1.get();
+            l2.growBambooSection(bambooSection1);
+        }
+        var res2 = this.bambooReserve.stream().filter(b -> b.getColor().equals(l2.getColor())).findAny();
+        if (res2.isEmpty())
+        {
+            return;
+        }
+        var bambooSection2 = res2.get();
+        assertFalse(l2.growBambooSection(bambooSection2));
+    }
+
+    @Test
+    void growBambooSectionThrowsException()
+    {
+        var res = this.bambooReserve.stream().filter(b -> b.getColor().equals(Color.GREEN)).findAny();
+        if (res.isEmpty())
+        {
+            return;
+        }
+        var bambooSection = res.get();
+        assertThrows(IllegalArgumentException.class, () -> l2.growBambooSection(bambooSection));
+
+        assertThrows(IllegalArgumentException.class, () -> l2.growBambooSection(null));
     }
 }
