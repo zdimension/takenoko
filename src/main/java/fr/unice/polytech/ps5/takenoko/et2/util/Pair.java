ee823a0398d99dbb9e04e7003231f865301f826e
Thomas Prévost <thomas.prevost@etu.univ-cotedazur.fr>
2020-12-07 13:22:29 +0100

    Pair: javadoc ok

--- src/main/java/fr/unice/polytech/ps5/takenoko/et2/util/Pair.java
+++ src/main/java/fr/unice/polytech/ps5/takenoko/et2/util/Pair.java
@@ -1,7 +1,7 @@
 package fr.unice.polytech.ps5.takenoko.et2.util;
 
 /**
- * Generic Pair class
+ * Generic Pair class (God bless C++)
  */
 public class Pair<T1, T2>
 {
@@ -14,6 +14,15 @@ public class Pair<T1, T2>
         this.second = second;
     }
 
+    /**
+     * Generate a new Pair
+     *
+     * @param first  First element to be paired
+     * @param second Second element to be paired
+     * @param <T1>   Class of the first element
+     * @param <T2>   Class of the second element
+     * @return The pair of [first, second]
+     */
     public static <T1, T2> Pair<T1, T2> of(T1 first, T2 second)
     {
         return new Pair<>(first, second);
@@ -24,12 +33,23 @@ public class Pair<T1, T2>
         return o == null ? 0 : o.hashCode();
     }
 
+    /**
+     * Get the hashcode
+     *
+     * @return Hashcode of the Pair (first and second)
+     */
     @Override
     public int hashCode()
     {
         return 31 * hashCode(first) + hashCode(second);
     }
 
+    /**
+     * Check if the Object obj is equal to this Pair
+     *
+     * @param obj Object to be compared
+     * @return true if obj is instance of pair and his 2 elements are equals, false otherwise
+     */
     @Override
     public boolean equals(Object obj)
     {
@@ -51,17 +71,32 @@ public class Pair<T1, T2>
         return o1 == null ? o2 == null : (o1 == o2 || o1.equals(o2));
     }
 
+    /**
+     * Convert the pair into a String
+     *
+     * @return The String representing the Pair
+     */
     @Override
     public String toString()
     {
         return "(" + first + ", " + second + ')';
     }
 
+    /**
+     * Get first element of the Pair
+     *
+     * @return The first element of the Pair
+     */
     public T1 getFirst()
     {
         return first;
     }
 
+    /**
+     * Get second element of the Pair
+     *
+     * @return The second element of the Pair
+     */
     public T2 getSecond()
     {
         return second;
