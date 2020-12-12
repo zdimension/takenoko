9c7afe1024648ae90ca8116abf2df1bc362c3a16
Emmeline Vouriot <emmeline.vouriot@etu.univ-cotedazur.fr>
2020-12-12 20:20:27 +0100

    add board test
    will work when add watershed will work

--- src/test/java/fr/unice/polytech/ps5/takenoko/et2/board/BoardTest.java
+++ src/test/java/fr/unice/polytech/ps5/takenoko/et2/board/BoardTest.java
@@ -4,6 +4,7 @@ import fr.unice.polytech.ps5.takenoko.et2.enums.Color;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -67,5 +68,59 @@ class BoardTest
                 addTileInfo(map, np, neighbor);
             }
         }
+    }
+
+    @Test
+    void testGetBambooableTiles()
+    {
+        Board board = new Board();
+        LandTile l1 = new LandTile(Color.GREEN);
+        LandTile l2 = new LandTile(Color.YELLOW);
+        LandTile l3 = new LandTile(Color.PINK);
+        LandTile l4 = new LandTile(Color.PINK);
+        LandTile l5 = new LandTile(Color.GREEN);
+        LandTile l6 = new LandTile(Color.YELLOW);
+        LandTile l7 = new LandTile(Color.YELLOW);
+        LandTile l8 = new LandTile(Color.YELLOW);
+
+
+
+        board.addTile(l1, new TilePosition(0, 1));
+        board.addTile(l2, new TilePosition(1, 0));
+        board.addTile(l3, new TilePosition(1, -1));
+        board.addTile(l4, new TilePosition(1, 1));
+        board.addTile(l5, new TilePosition(2, -1));
+        board.addTile(l6, new TilePosition(2, 0));
+        board.addTile(l7, new TilePosition(2, 1));
+        board.addTile(l8, new TilePosition(0, 2));
+
+        l1.getEdge(2).addIrrigation();
+        l1.getEdge(1).addIrrigation();
+        l1.getEdge(0).addIrrigation();
+        l1.cutBambooSection();
+        l2.getEdge(0).addIrrigation();
+        l5.setLandTileImprovement(LandTileImprovement.WATERSHED);
+        l7.setLandTileImprovement(LandTileImprovement.WATERSHED);
+        l7.cutBambooSection();
+        l4.growBambooSection();
+        l4.growBambooSection();
+        l4.growBambooSection();
+        l4.growBambooSection();
+        l3.growBambooSection();
+        l3.growBambooSection();
+        l3.growBambooSection();
+        l5.growBambooSection();
+        l5.growBambooSection();
+        l5.growBambooSection();
+        l8.growBambooSection();
+        l8.growBambooSection();
+
+        var expectedResult = new HashSet<LandTile>();
+        expectedResult.add(l1);
+        expectedResult.add(l2);
+        expectedResult.add(l7);
+        expectedResult.add(l8);
+
+        assertEquals(expectedResult, new HashSet<>(board.getBambooableTiles()));
     }
 }
