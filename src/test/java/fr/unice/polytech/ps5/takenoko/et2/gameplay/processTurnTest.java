4d4c60e74b2e7adbbca3e7471db0c4c232e73371
Emmeline Vouriot <emmeline.vouriot@etu.univ-cotedazur.fr>
2020-12-12 20:20:44 +0100

    add test skeleton

--- src/test/java/fr/unice/polytech/ps5/takenoko/et2/gameplay/processTurnTest.java
+++ src/test/java/fr/unice/polytech/ps5/takenoko/et2/gameplay/processTurnTest.java
@@ -1,4 +1,31 @@
+package fr.unice.polytech.ps5.takenoko.et2.gameplay;
+
+import fr.unice.polytech.ps5.takenoko.et2.GameData;
+import fr.unice.polytech.ps5.takenoko.et2.decision.DecisionMakerBuilder;
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.*;
-class processTurnTest {
-  
+import static org.mockito.Mockito.mock;
+
+class processTurnTest
+{
+    private Game game;
+    private DecisionMakerBuilder mockBuilder1;
+    private DecisionMakerBuilder mockBuilder2;
+
+
+    @BeforeEach
+    void init() throws IllegalAccessException
+    {
+        game = GameData.getStandardGame();
+
+        mockBuilder1 = mock(DecisionMakerBuilder.class);
+        mockBuilder2 = mock(DecisionMakerBuilder.class);
+
+        game.addPlayer(mockBuilder1);
+        game.addPlayer(mockBuilder2);
+    }
+
+
+
 }
