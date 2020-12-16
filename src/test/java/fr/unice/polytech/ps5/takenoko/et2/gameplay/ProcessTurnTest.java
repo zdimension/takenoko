b2269b9a2e1e99660d1213645167854b50f10d91
Emmeline Vouriot <emmeline.vouriot@etu.univ-cotedazur.fr>
2020-12-16 16:34:04 +0100

    add test

--- src/test/java/fr/unice/polytech/ps5/takenoko/et2/gameplay/ProcessTurnTest.java
+++ src/test/java/fr/unice/polytech/ps5/takenoko/et2/gameplay/ProcessTurnTest.java
@@ -116,11 +116,11 @@ class ProcessTurnTest
         gameActionList1 = new ArrayList<>(new ArrayList<>(Arrays.asList(GameAction.values())));
         gameActionList1.remove(GameAction.COMPLETE_OBJECTIVE);
         gameActionList1.remove(GameAction.PLACE_IRRIGATION);
-        gameActionList1.remove(GameAction.PLACE_IMPROVEMENT); //TODO remove this line when chipReserve intanciated
+        gameActionList1.remove(GameAction.PLACE_IMPROVEMENT);
         gameActionList2 = new ArrayList<>(new ArrayList<>(Arrays.asList(GameAction.values())));
         gameActionList2.remove(GameAction.COMPLETE_OBJECTIVE);
         gameActionList2.remove(GameAction.PLACE_IRRIGATION);
-        gameActionList2.remove(GameAction.PLACE_IMPROVEMENT); //TODO remove this line when chipReserve intanciated
+        gameActionList2.remove(GameAction.PLACE_IMPROVEMENT);
         gameActionList2.remove(GameAction.DRAW_OBJECTIVE);
         gameActionList3 = new ArrayList<>(new ArrayList<>(GameAction.getUnlimitedActions()));
         gameActionList3.add(null);
@@ -149,7 +149,53 @@ class ProcessTurnTest
     }
 
     @Test
-    void checkRainButDecisionsMakerReturnsInvalidTilesAndItActsLikeNull()
+    void checkRainButDecisionsMakerReturnsInvalidTilesAndItActsLikeNull() throws DecisionMakerException, IllegalAccessException
     {
+        game = new Game(new Random(999999992));
+        p1 = game.addPlayer(p -> mockDecisionMaker1);
+        p2 = game.addPlayer(p -> mockDecisionMaker2);
+
+        game.isFirstRound = false;
+        var t1 = new LandTile(Color.YELLOW);
+        game.getBoard().addTile(t1 , new TilePosition(0, 1));
+
+        when(p1.getDecisionMaker().chooseTileToAddBamboo(game.getBoard().getBambooableTiles())).thenReturn(new LandTile(Color.YELLOW));
+
+        List<GameAction> gameActionList1;
+        List<GameAction> gameActionList2;
+        List<GameAction> gameActionList3;
+        gameActionList1 = new ArrayList<>(new ArrayList<>(Arrays.asList(GameAction.values())));
+        gameActionList1.remove(GameAction.COMPLETE_OBJECTIVE);
+        gameActionList1.remove(GameAction.PLACE_IRRIGATION);
+        gameActionList1.remove(GameAction.PLACE_IMPROVEMENT);
+        gameActionList2 = new ArrayList<>(new ArrayList<>(Arrays.asList(GameAction.values())));
+        gameActionList2.remove(GameAction.COMPLETE_OBJECTIVE);
+        gameActionList2.remove(GameAction.PLACE_IRRIGATION);
+        gameActionList2.remove(GameAction.PLACE_IMPROVEMENT);
+        gameActionList2.remove(GameAction.DRAW_OBJECTIVE);
+        gameActionList3 = new ArrayList<>(new ArrayList<>(GameAction.getUnlimitedActions()));
+        gameActionList3.add(null);
+
+        when(p1.getDecisionMaker().chooseAction(gameActionList1)).thenReturn(GameAction.DRAW_OBJECTIVE);
+        when(p1.getDecisionMaker().chooseAction(gameActionList2)).thenReturn(GameAction.PICK_IRRIGATION);
+        when(p1.getDecisionMaker().chooseAction(gameActionList3)).thenReturn(null);
+
+        Class<?> cLass = PlotObjective.class;
+        Mockito.<Class<?>>when(p1.getDecisionMaker().chooseDeck(game.gameData.objectiveDecks
+            .entrySet()
+            .stream()
+            .filter(e -> !e.getValue().isEmpty())
+            .map(Map.Entry::getKey)
+            .collect(Collectors.toUnmodifiableList())
+        )).thenReturn(cLass);
+
+        assertTrue(game.processTurn(p1));
+
+        var board = new Board();
+        var t2 = new LandTile(Color.YELLOW);
+        board.addTile(t2,  new TilePosition(0, 1));
+        var truc = board.equals(game.getBoard());
+
+        assertEquals(board, game.getBoard());
     }
 }
