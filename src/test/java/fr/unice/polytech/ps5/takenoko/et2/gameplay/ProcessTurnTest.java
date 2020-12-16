5fba50e118f0f6dad3dc65f0183fdedb0d78d1dc
Emmeline Vouriot <emmeline.vouriot@etu.univ-cotedazur.fr>
2020-12-16 13:55:34 +0100

    typo fixed

--- src/test/java/fr/unice/polytech/ps5/takenoko/et2/gameplay/ProcessTurnTest.java
+++ src/test/java/fr/unice/polytech/ps5/takenoko/et2/gameplay/ProcessTurnTest.java
@@ -21,7 +21,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class processTurnTest
+class ProcessTurnTest
 {
     private Game game;
 
