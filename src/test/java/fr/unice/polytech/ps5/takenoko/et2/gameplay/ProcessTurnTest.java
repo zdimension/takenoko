260a6d72a5b04332a0b51b1963571a04663aed05
Emmeline Vouriot <emmeline.vouriot@etu.univ-cotedazur.fr>
2020-12-16 16:21:31 +0100

    Repair + add test on weather

--- src/test/java/fr/unice/polytech/ps5/takenoko/et2/gameplay/ProcessTurnTest.java
+++ src/test/java/fr/unice/polytech/ps5/takenoko/et2/gameplay/ProcessTurnTest.java
@@ -15,6 +15,8 @@ import org.mockito.Mockito;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -32,27 +34,81 @@ class ProcessTurnTest
     @BeforeEach
     void init() throws IllegalAccessException
     {
-        game = new Game(new Random(999999999));
-
         mockDecisionMaker1 = mock(DecisionMaker.class);
         mockDecisionMaker2 = mock(DecisionMaker.class);
-        p1 = game.addPlayer(p -> mockDecisionMaker1);
-        p2 = game.addPlayer(p -> mockDecisionMaker2);
-
     }
 
     @Test
-    void checkstandardRain() throws DecisionMakerException
+    void testQuestionMarkAndRainStandartUsageEverythingOk() throws DecisionMakerException, IllegalAccessException
     {
+        game = new Game(new Random(999999999));
+        p1 = game.addPlayer(p -> mockDecisionMaker1);
+        p2 = game.addPlayer(p -> mockDecisionMaker2);
+
         game.isFirstRound = false;
         var t1 = new LandTile(Color.YELLOW);
         game.getBoard().addTile(t1 , new TilePosition(0, 1));
         List<Weather> weatherList = new ArrayList<>(Arrays.asList(Weather.values()));
         weatherList.remove(Weather.QUESTION_MARK);
-        weatherList.remove(Weather.CLOUDS);
 
         when(p1.getDecisionMaker().chooseWeather(weatherList)).thenReturn(Weather.RAIN);
         when(p1.getDecisionMaker().chooseTileToAddBamboo(game.getBoard().getBambooableTiles())).thenReturn(t1);
+
+        List<GameAction> gameActionList1;
+        List<GameAction> gameActionList2;
+        List<GameAction> gameActionList3;
+        gameActionList1 = new ArrayList<>(new ArrayList<>(Arrays.asList(GameAction.values())));
+        gameActionList1.remove(GameAction.COMPLETE_OBJECTIVE);
+        gameActionList1.remove(GameAction.PLACE_IRRIGATION);
+        gameActionList1.remove(GameAction.PLACE_IMPROVEMENT);
+        gameActionList2 = new ArrayList<>(new ArrayList<>(Arrays.asList(GameAction.values())));
+        gameActionList2.remove(GameAction.COMPLETE_OBJECTIVE);
+        gameActionList2.remove(GameAction.PLACE_IRRIGATION);
+        gameActionList2.remove(GameAction.PLACE_IMPROVEMENT);
+        gameActionList2.remove(GameAction.DRAW_OBJECTIVE);
+        gameActionList3 = new ArrayList<>(new ArrayList<>(GameAction.getUnlimitedActions()));
+        gameActionList3.add(null);
+
+        when(p1.getDecisionMaker().chooseAction(gameActionList1)).thenReturn(GameAction.DRAW_OBJECTIVE);
+        when(p1.getDecisionMaker().chooseAction(gameActionList2)).thenReturn(GameAction.PICK_IRRIGATION);
+        when(p1.getDecisionMaker().chooseAction(gameActionList3)).thenReturn(null);
+
+        Class<?> cLass = PlotObjective.class;
+        Mockito.<Class<?>>when(p1.getDecisionMaker().chooseDeck(game.gameData.objectiveDecks
+            .entrySet()
+            .stream()
+            .filter(e -> !e.getValue().isEmpty())
+            .map(Map.Entry::getKey)
+            .collect(Collectors.toUnmodifiableList())
+        )).thenReturn(cLass);
+
+        assertTrue(game.processTurn(p1));
+
+        var board = new Board();
+        var t2 = new LandTile(Color.YELLOW);
+        board.addTile(t2,  new TilePosition(0, 1));
+        t2.growBambooSection();
+        // todo: broken since improvements added
+        assertEquals(board, game.getBoard());
+    }
+
+    @Test
+    void testRainNothingHappendsButItsNormal()
+    {
+    }
+
+    @Test
+    void testRainButDecisionMakerChoosesNotToDoAnything() throws IllegalAccessException, DecisionMakerException
+    {
+        game = new Game(new Random(999999992));
+        p1 = game.addPlayer(p -> mockDecisionMaker1);
+        p2 = game.addPlayer(p -> mockDecisionMaker2);
+
+        game.isFirstRound = false;
+        var t1 = new LandTile(Color.YELLOW);
+        game.getBoard().addTile(t1 , new TilePosition(0, 1));
+
+        when(p1.getDecisionMaker().chooseTileToAddBamboo(game.getBoard().getBambooableTiles())).thenReturn(null);
 
         List<GameAction> gameActionList1;
         List<GameAction> gameActionList2;
@@ -82,16 +138,18 @@ class ProcessTurnTest
             .collect(Collectors.toUnmodifiableList())
         )).thenReturn(cLass);
 
-
-        //assertTrue(game.processTurn(p1));
+        assertTrue(game.processTurn(p1));
 
         var board = new Board();
         var t2 = new LandTile(Color.YELLOW);
         board.addTile(t2,  new TilePosition(0, 1));
-        t2.growBambooSection();
         var truc = board.equals(game.getBoard());
-        // todo: broken since improvements added
-        //assertEquals(board, game.getBoard());
 
+        assertEquals(board, game.getBoard());
+    }
+
+    @Test
+    void checkRainButDecisionsMakerReturnsInvalidTilesAndItActsLikeNull()
+    {
     }
 }
