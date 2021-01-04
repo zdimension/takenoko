1fb160faac1a85d59bd0a1802daaec5aa4a9834a
Emmeline Vouriot <emmeline.vouriot@etu.univ-cotedazur.fr>
2021-01-04 16:16:30 +0100

    add weather test

--- src/test/java/fr/unice/polytech/ps5/takenoko/et2/gameplay/ProcessTurnTest.java
+++ src/test/java/fr/unice/polytech/ps5/takenoko/et2/gameplay/ProcessTurnTest.java
@@ -505,4 +505,85 @@ class ProcessTurnTest
         assertEquals(board, game.getBoard());
         assertEquals(0, p1.getBambooSectionReserve().get(Color.YELLOW));
     }
+
+    @Test
+    void testStormPandaMovesToPond() throws DecisionMakerException, IllegalAccessException
+    {
+        game = new Game(new Random(999999990));
+        p1 = game.addPlayer(p -> mockDecisionMaker1);
+        p2 = game.addPlayer(p -> mockDecisionMaker2);
+
+        game.isFirstRound = false;
+        var t1 = new LandTile(Color.YELLOW);
+        var t2 = new LandTile(Color.YELLOW);
+        game.getBoard().addTile(t1 , new TilePosition(0, 1));
+        game.getBoard().addTile(t2 , new TilePosition(-1, 0));
+        t2.cutBambooSection();
+
+
+        var PandaPositionAvailableList = new ArrayList<TilePosition>()
+        {{
+            addAll(game.getBoard().getTiles().keySet());
+        }};
+
+        when(p1.getDecisionMaker().choosePandaTarget(PandaPositionAvailableList, true)).thenReturn(t2.getPosition().get(), game.getBoard().getCenter().getPosition().get());
+
+        List<GameAction> gameActionList1;
+        List<GameAction> gameActionList2;
+        List<GameAction> gameActionList3;
+        List<GameAction> gameActionList4;
+        List<GameAction> gameActionList5;
+        gameActionList1 = new ArrayList<>(new ArrayList<>(Arrays.asList(GameAction.values())));
+        gameActionList1.remove(GameAction.COMPLETE_OBJECTIVE);
+        gameActionList1.remove(GameAction.PLACE_IRRIGATION);
+        gameActionList1.remove(GameAction.PLACE_IMPROVEMENT);
+        gameActionList2 = new ArrayList<>(new ArrayList<>(Arrays.asList(GameAction.values())));
+        gameActionList2.remove(GameAction.COMPLETE_OBJECTIVE);
+        gameActionList2.remove(GameAction.PLACE_IRRIGATION);
+        gameActionList2.remove(GameAction.PLACE_IMPROVEMENT);
+        gameActionList2.remove(GameAction.DRAW_OBJECTIVE);
+        gameActionList3 = new ArrayList<>(new ArrayList<>(GameAction.getUnlimitedActions()));
+        gameActionList3.add(null);
+        gameActionList4 = new ArrayList<>(new ArrayList<>(Arrays.asList(GameAction.values())));
+        gameActionList4.remove(GameAction.COMPLETE_OBJECTIVE);
+        gameActionList4.remove(GameAction.PLACE_IMPROVEMENT);
+        gameActionList5 = new ArrayList<>(new ArrayList<>(Arrays.asList(GameAction.values())));
+        gameActionList5.remove(GameAction.PLACE_IMPROVEMENT);
+        gameActionList5.remove(GameAction.COMPLETE_OBJECTIVE);
+        gameActionList5.remove(GameAction.DRAW_OBJECTIVE);
+
+        when(p1.getDecisionMaker().chooseAction(gameActionList1)).thenReturn(GameAction.DRAW_OBJECTIVE);
+        when(p1.getDecisionMaker().chooseAction(gameActionList2)).thenReturn(GameAction.PICK_IRRIGATION);
+        when(p1.getDecisionMaker().chooseAction(gameActionList4)).thenReturn(GameAction.DRAW_OBJECTIVE);
+        when(p1.getDecisionMaker().chooseAction(gameActionList5)).thenReturn(GameAction.PICK_IRRIGATION);
+        when(p1.getDecisionMaker().chooseAction(gameActionList3)).thenReturn(null, null);
+
+        Class<?> cLass = PlotObjective.class;
+        Mockito.<Class<?>>when(p1.getDecisionMaker().chooseDeck(game.gameData.objectiveDecks
+            .entrySet()
+            .stream()
+            .filter(e -> !e.getValue().isEmpty())
+            .map(Map.Entry::getKey)
+            .collect(Collectors.toUnmodifiableList())
+        )).thenReturn(cLass, cLass);
+
+        assertTrue(game.processTurn(p1));
+
+        List<Weather> weatherList = new ArrayList<>(Arrays.asList(Weather.values()));
+        weatherList.remove(Weather.QUESTION_MARK);
+        when(p1.getDecisionMaker().chooseWeather(weatherList)).thenReturn(Weather.STORM);
+        when(p1.getDecisionMaker().chooseTileToAddBamboo(game.getBoard().getBambooableTiles())).thenReturn(t1);
+
+        assertTrue(game.processTurn(p1));
+
+        var board = new Board();
+        var t3 = new LandTile(Color.YELLOW);
+        var t4 = new LandTile(Color.YELLOW);
+        board.addTile(t3,  new TilePosition(0, 1));
+        board.addTile(t4,  new TilePosition(-1, 0));
+        t4.cutBambooSection();
+
+        assertEquals(board, game.getBoard());
+        assertEquals(0, p1.getBambooSectionReserve().get(Color.YELLOW));
+    }
 }
