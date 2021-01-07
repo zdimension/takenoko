ab40599ac5dcfc62591896f1d644f158942af128
Tom Niget <tom.niget@etu.univ-cotedazur.fr>
2021-01-07 12:10:32 +0100

    Restrict annotations targets

--- src/main/java/fr/unice/polytech/ps5/takenoko/et2/commandline/annotations/Bot.java
+++ src/main/java/fr/unice/polytech/ps5/takenoko/et2/commandline/annotations/Bot.java
@@ -1,9 +1,12 @@
 package fr.unice.polytech.ps5.takenoko.et2.commandline.annotations;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface Bot
 {
     String key();
